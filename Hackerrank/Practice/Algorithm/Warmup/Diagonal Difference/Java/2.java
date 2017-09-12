4c298a034abb963f94c19c7066e0d8f7ee5f1d47
= <dhayal.ram@ei-india.com>
2017-09-12 20:54:37 +0530

    Diagonal Difference in java
    Adding better solution in java programming language

---
+++ Hackerrank/Practice/Algorithm/Warmup/Diagonal Difference/Java/2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int  size = in.nextInt(), temp, a=0,b=0;
+        for (int  x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                temp = in.nextInt();
+                if(x == y) a += temp; 
+                if(x+y == size-1) b += temp; 
+            }
+        }
+        System.out.println(Math.abs(a-b));
+    }
+}
