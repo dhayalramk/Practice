654d76d381948c8da48b6d7f4c337027ae79fb8d
= <dhayal.ram@ei-india.com>
2017-09-12 20:25:34 +0530

    Diagonal Difference php solution
    Adding Diagonal Difference soution in PHP

---
+++ Hackerrank/Practice/Algorithm/Warmup/Diagnoal Difference/PHP/1.php
@@ -0,0 +1,14 @@
+<?php
+
+	$lines = explode(PHP_EOL, file_get_contents('php://stdin'));
+	$count = array_shift($lines);
+	$a = $b = 0;
+
+	for ($i = 0; $i < $count; $i++){
+	  $nums = explode(' ', $lines[$i]);
+	  $a += $nums[$i];
+	  $b += $nums[$count - $i - 1];
+	}
+	echo abs($a - $b);
+
+?>
