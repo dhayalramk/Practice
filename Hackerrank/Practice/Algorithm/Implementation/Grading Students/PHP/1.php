a56dd16131ab275c24cd45ec41309192e72449b5
Dhayal Ram <dhayal.ram@ei-india.com>
2017-09-13 22:01:31 +0530

    Grading Students solution in php
    Adding Grading students solutions in PHP

---
+++ Hackerrank/Practice/Algorithm/Implementation/Grading Students/PHP/1.php
@@ -0,0 +1,5 @@
+<?php
+    $arr = explode(PHP_EOL,file_get_contents("php://stdin"));
+    for($i=1;$i<=$arr[0];$i++) 
+        echo ( ($arr[$i] < 38 || round($arr[$i] % 5) < 3 ) ? $arr[$i] : (5 * round($arr[$i] / 5))).PHP_EOL;
+?>
