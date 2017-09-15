f3872e468e7212966495f268b5900d2e2fe817f9
Dhayal Ram <dhayal.ram@ei-india.com>
2017-09-16 00:07:27 +0530

    Solution for Day of the Programmer
    Adding solution for Day of the Programming in PHP Programming Language

---
+++ Hackerrank/Practice/Algorithm/Implementation/Day of the Programmer/PHP/1.php
@@ -0,0 +1,32 @@
+<?php
+    $date = 1;
+    $month = 0;
+    $year = file_get_contents('php://stdin');
+
+    $total = 256;
+
+    $followYear = [];
+    $feb = 28;
+    
+    if($year == 1918){
+        $feb = $feb-13;
+    }else if( ($year < 1918 && $year%4==0 )
+        || ($year%400==0 || ($year%4==0 && $year%100!=0) ) ){
+        $feb++;
+    }
+
+    $followYear = [31,$feb,31,30,31,30,31,31,30,31,30,31];
+
+    for($i=0;$i<count($followYear);$i++){
+        $month++;
+        if($total - $followYear[$i] <= 0){
+            $date = $total;
+            break;
+        }
+        $total = $total - $followYear[$i];
+    }
+
+    echo DateTime::createFromFormat('d-m-Y', implode('-',[$date,$month,$year]))->format('d.m.Y');
+
+
+?>
