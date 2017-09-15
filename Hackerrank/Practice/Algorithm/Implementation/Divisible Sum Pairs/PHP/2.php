257e24660436ab225f1ebb76d74a77547dcc23d7
Dhayal Ram <dhayalram.k@gmail.com>
2017-09-16 00:13:26 +0530

    Divisible Sum pairs Solution 2
    Adding better solution for Divisiblee Sum Pairs

---
+++ Hackerrank/Practice/Algorithm/Implementation/Divisible Sum Pairs/PHP/2.php
@@ -0,0 +1,14 @@
+<?php
+
+    $lines = explode(PHP_EOL, file_get_contents("php://stdin"));
+    list($n,$k) = explode(' ', $lines[0]);
+    $arr = explode(' ', $lines[1]);
+    $count = 0;
+
+    for($i=0;$i<$n-1;$i++)
+        for( $j = $i+1; $j < $n; $j++ ) 
+            $count += ($arr[$i]+$arr[$j]) % $k == 0 ? 1 : 0;
+ 		   
+    echo $count . "\n";
+ 
+?>
