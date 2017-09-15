b89be6c9f8d1afa38e613bd6411ea164c0cc71a1
Dhayal Ram <dhayal.ram@ei-india.com>
2017-09-16 00:06:53 +0530

    Solution for Counting Valleys
    Adding solution for Counting Valleys in PHP Programming language

---
+++ Hackerrank/Practice/Algorithm/Implementation/Counting Valleys/PHP/1.php
@@ -0,0 +1,19 @@
+<?php
+    
+$arr=str_split(explode(PHP_EOL,file_get_contents('php://stdin'))[1]);
+$count = 0;
+$level = 1;
+$isDown = false;
+foreach($arr as $a){
+    $a == 'U' ? $level+=1 : $level-=1;
+    if($isDown && $level == 1){
+        $isDown = false;
+        $count++;
+    }   
+    if($level == 0){
+        $isDown = true;
+    }
+}
+echo $count;
+
+?>
