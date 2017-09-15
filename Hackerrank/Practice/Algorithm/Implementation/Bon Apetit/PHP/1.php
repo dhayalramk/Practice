365f1bf9d9aad13fcb8b479af0f735ab99ffa308
Dhayal Ram <dhayal.ram@ei-india.com>
2017-09-16 00:05:42 +0530

    Bon Apetit Solution in PHP
    Adding solution for Bon Apetit in PHP Programming

---
+++ Hackerrank/Practice/Algorithm/Implementation/Bon Apetit/PHP/1.php
@@ -0,0 +1,10 @@
+<?php
+    $lines = explode(PHP_EOL,file_get_contents('php://stdin'));
+    list($n,$k) = explode(' ',$lines[0]);
+    $arr = explode(' ' , $lines[1]);
+    $paid = $lines[2];
+
+    unset($arr[$k]);
+    $total = array_sum($arr) / 2;
+    echo ($total == $paid) ? 'Bon Appetit' : $paid - $total;
+?>
