fea77d25cadeb2b0556d3e923c01ba2b5da007f9
Dhayal Ram <dhayalram.k@gmail.com>
2017-09-16 00:09:41 +0530

    Indendation

--- Hackerrank/Practice/Algorithm/Implementation/Bon Apetit/PHP/1.php
+++ Hackerrank/Practice/Algorithm/Implementation/Bon Apetit/PHP/1.php
@@ -3,7 +3,7 @@
     list($n,$k) = explode(' ',$lines[0]);
     $arr = explode(' ' , $lines[1]);
     $paid = $lines[2];
-
+ 
     unset($arr[$k]);
     $total = array_sum($arr) / 2;
     echo ($total == $paid) ? 'Bon Appetit' : $paid - $total;
