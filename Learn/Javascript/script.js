76c4b15c0d9549e6c3ff75c079c337ac4c121607
Dhayal Ram <dhayalram.k@gmail.com>
2017-09-20 22:08:28 +0530

    Changes

--- Learn/Javascript/script.js
+++ Learn/Javascript/script.js
@@ -3,3 +3,9 @@ var str = 'string';
 var boo1 = true;
 var boo2 = false;
 var num2 = 5.6;
+
+console.log(num1); // 5
+console.log(str); // "string"
+console.log(boo1); // true
+console.log(boo2); // false
+console.log(num2); // 5.6
